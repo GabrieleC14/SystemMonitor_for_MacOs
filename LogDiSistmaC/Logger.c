714d9b196fe20beb44c83ac0289208402808db5e
Gabriele Cifuni <153102633+GabrieleC14@users.noreply.github.com>
2025-06-10 16:33:44 +0200

    Update LoggerC

--- LogDiSistmaC/Logger.c
+++ LogDiSistmaC/Logger.c
@@ -5,10 +5,27 @@
 
 int main(){
 
-    int numberReturn  = 999;
+    FILE *fp;
+    char buffer[1024];
+    char command[256];
 
-    printf("Ciao esisto");
+    // Esempio: Interroga i log da com.apple.xpc.launchd degli ultimi 5 minuti
+    snprintf(command, sizeof(command), "log show --last 5m --predicate 'subsystem == \"com.apple.xpc.launchd\"' --info --debug");
 
-    return numberReturn;
+    fp = popen(command, "r");
+    if (fp == NULL) {
+        perror("Impossibile eseguire il comando");
+        return 1;
+    }
+
+    printf("--- Output da 'log show' ---\n");
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        printf("%s", buffer);
+    }
+    printf("----------------------------\n");
+
+    pclose(fp); // Chiudi la pipe
+
+    return 0;
 
 }
